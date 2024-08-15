0c1a15587d25d8bbbd7c97d6c66767d9b505f27a
FIRDAVS00222 <shoirajamilova5@gmail.com>
2024-08-15 13:19:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let ism = prompt('Ismingizni kiriting?')
+console.log('Ism: ' + ism);
+
+let familya = prompt('Familyangizni kiriting?')
+console.log('Familya: ' + familya);
+
+let yosh = prompt('Yoshingizni kiriting?')
+console.log('Yosh: ' + yosh);
+
+let Fan = prompt('Yoqtirgan faningizni kiriting?')
+console.log('FAN: ' + Fan );
+
+let tuman = prompt('Yashaydigan tumaningizni kiriting?')
+console.log('tuman: ' + tuman);
+
+let javob = ('Javob berganingiz uchun raxmat!')
+alert( javob);
